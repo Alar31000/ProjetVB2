fc63c980cc9b8d4b1300a8df2c73985ff04bb133
Yaseen Choukri <52813353+Yassotaha@users.noreply.github.com>
2021-04-11 10:43:36 -0400

    Data base Consultet fonctionne bien!

--- WindowsApp1/FrmConsulterContrat.vb
+++ WindowsApp1/FrmConsulterContrat.vb
@@ -8,23 +8,6 @@ Public Class FrmConsulterContrat
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
-
-    Private Sub datagridShow()
-        Dim objDs As New DataSet
-        Dim objDt As New DataTable
-        objDs.Tables.Add(objDt)
-        'Dim objDa As New OleDbDataAdapter
-
-        'objDt = New OleDbDataAdapter("select * from CLIENT, SOUSCRIT, CONTRAT where CLIENT.NUMCLIENT = SOUSCRIT.NUMCLIENT and CONTRAT.NUMCONTRAT = SOUSCRIT.NUMCONTRAT", objCnn)
-        'objDa = New OleDbDataAdapter("Select CLIENT.*, CONTRAT.*From CONTRAT INNER Join (CLIENT INNER Join SOUSCRIT On CLIENT.NUMCLIENT = SOUSCRIT.NUMCLIENT) ON CONTRAT.NUMCONTRAT = SOUSCRIT.NUMCONTRAT", objCnn)
-
-        'objDa.Fill(objDt)
-
-        ' DataGridView1.DataSource = objDt.DefaultView
-
-        ' objCnn.Close()
-
-    End Sub
     Private Sub Titre_Click(sender As Object, e As EventArgs) Handles Titre.Click
 
     End Sub
@@ -39,54 +22,46 @@ Public Class FrmConsulterContrat
         connectionString = String.Format("Data Source={0};Version=3;",
                                          Directory.GetCurrentDirectory() & "\" & "usersDb.db")
 
-
-
-        'Dim sqlContrat As String = "SELECT * FROM Contrat"
-
+        ' Pour affichier la table Client dans le premier DataGrid
         Using conn As New SQLiteConnection(connectionString.ToString)
-                Using cmd As New SQLiteCommand(conn)
+            Using cmd As New SQLiteCommand(conn)
 
-                cmd.CommandText = "SELECT * FROM Clients;"
-                'cmd.CommandText = sqlContrat
+                cmd.CommandText = "SELECT * FROM Clients"
+
 
                 conn.Open()
 
-                    Using da As New SQLiteDataAdapter(cmd)
-                        'Using da As New SQLiteDataAdapter(cmd)
-                        Dim dt As New DataTable
+                Using da As New SQLiteDataAdapter(cmd)
+                    'Using da As New SQLiteDataAdapter(cmd)
+                    Dim dt As New DataTable
 
 
-                        DataGridView1.DataSource = dt
-                        da.Fill(dt)
-                        ' DataGridView2.DataSource = dt
-                        ' da.Fill(dt)
+                    DataGridView1.DataSource = dt
 
-
-                    End Using
+                    da.Fill(dt)
                 End Using
             End Using
+        End Using
 
-        'TODO: cette ligne de code charge les données dans la table 'SgariBddDataSet.CONTRAT'. Vous pouvez la déplacer ou la supprimer selon les besoins.
-        'Me.CONTRATTableAdapter.Fill(Me.SgariBddDataSet.CONTRAT)
-        'TODO: cette ligne de code charge les données dans la table 'SgariBddDataSet.CLIENT'. Vous pouvez la déplacer ou la supprimer selon les besoins.
-        ' Me.CLIENTTableAdapter.Fill(Me.SgariBddDataSet.CLIENT)
-
-        'objDs = New DataSet
-        'objTable = objDs.Tables
-        'objDa = New OleDb.OleDbDataAdapter("select * from CLIENT, SOUSCRIT, CONTRAT where CLIENT.NUMCLIENT = SOUSCRIT.NUMCLIENT and CONTRAT.NUMCONTRAT = SOUSCRIT.NUMCONTRAT", objCnn)
-        'objDa.Fill(objDs, "Client, contrat")
-        'objCmd.CommandType = System.Data.CommandType.Text
-
-        'objCnn.Open()
-        'datagridShow()
-        'objCmd.CommandText = "select * from CLIENT, SOUSCRIT, CONTRAT where CLIENT.NUMCLIENT = SOUSCRIT.NUMCLIENT and CONTRAT.NUMCONTRAT = SOUSCRIT.NUMCONTRAT"
-        '"Select CLIENT.*, CONTRAT.*From CONTRAT INNER Join (CLIENT INNER Join SOUSCRIT On CLIENT.NUMCLIENT = SOUSCRIT.NUMCLIENT) ON CONTRAT.NUMCONTRAT = SOUSCRIT.NUMCONTRAT"
+        ' Pour affichier la table Contrat dans la deuxieme DataGrid
+        Using conn As New SQLiteConnection(connectionString.ToString)
+            Using cmd2 As New SQLiteCommand(conn)
 
 
-        'objCmd.ExecuteNonQuery()
-        'Dim objView As New DataView(objTable(0))
+                cmd2.CommandText = "SELECT * FROM Contrat"
 
-        ' Fermeture de la base de données
+                conn.Open()
+
+                Using da As New SQLiteDataAdapter(cmd2)
+                    'Using da As New SQLiteDataAdapter(cmd)
+                    Dim dt As New DataTable
+                    DataGridView2.DataSource = dt
+                    da.Fill(dt)
+
+
+                End Using
+            End Using
+        End Using
 
     End Sub
 
