f82b1fd02b8b28c44e575b6e28c7c797f7643cd0
Sdiall <72425955+Sdiall@users.noreply.github.com>
2021-04-10 20:16:01 -0700

    sertyu

--- WindowsApp1/Renouveler Contrat.Designer.vb
+++ WindowsApp1/Renouveler Contrat.Designer.vb
@@ -48,11 +48,9 @@ Partial Class Renouveler_Contrat
         Me.GroupBox2.Controls.Add(Me.LblNumContratAModifier)
         Me.GroupBox2.Controls.Add(Me.BtnRechercher)
         Me.GroupBox2.Controls.Add(Me.TxtBoxNumCnt)
-        Me.GroupBox2.Location = New System.Drawing.Point(9, 274)
-        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Location = New System.Drawing.Point(7, 223)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox2.Size = New System.Drawing.Size(989, 121)
+        Me.GroupBox2.Size = New System.Drawing.Size(742, 98)
         Me.GroupBox2.TabIndex = 45
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Rechercher un contrat en particulier"
@@ -61,20 +59,18 @@ Partial Class Renouveler_Contrat
         '
         Me.LblNumContratAModifier.AutoSize = True
         Me.LblNumContratAModifier.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblNumContratAModifier.Location = New System.Drawing.Point(503, 32)
-        Me.LblNumContratAModifier.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LblNumContratAModifier.Location = New System.Drawing.Point(377, 26)
         Me.LblNumContratAModifier.Name = "LblNumContratAModifier"
-        Me.LblNumContratAModifier.Size = New System.Drawing.Size(172, 24)
+        Me.LblNumContratAModifier.Size = New System.Drawing.Size(136, 18)
         Me.LblNumContratAModifier.TabIndex = 21
         Me.LblNumContratAModifier.Text = "Numéro du contrat "
         '
         'BtnRechercher
         '
         Me.BtnRechercher.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnRechercher.Location = New System.Drawing.Point(727, 73)
-        Me.BtnRechercher.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnRechercher.Location = New System.Drawing.Point(545, 59)
         Me.BtnRechercher.Name = "BtnRechercher"
-        Me.BtnRechercher.Size = New System.Drawing.Size(156, 28)
+        Me.BtnRechercher.Size = New System.Drawing.Size(117, 23)
         Me.BtnRechercher.TabIndex = 34
         Me.BtnRechercher.Text = "Rechercher"
         Me.BtnRechercher.UseVisualStyleBackColor = True
@@ -82,10 +78,9 @@ Partial Class Renouveler_Contrat
         'TxtBoxNumCnt
         '
         Me.TxtBoxNumCnt.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TxtBoxNumCnt.Location = New System.Drawing.Point(727, 25)
-        Me.TxtBoxNumCnt.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TxtBoxNumCnt.Location = New System.Drawing.Point(545, 20)
         Me.TxtBoxNumCnt.Name = "TxtBoxNumCnt"
-        Me.TxtBoxNumCnt.Size = New System.Drawing.Size(253, 29)
+        Me.TxtBoxNumCnt.Size = New System.Drawing.Size(191, 24)
         Me.TxtBoxNumCnt.TabIndex = 22
         '
         'GroupBox3
@@ -93,21 +88,18 @@ Partial Class Renouveler_Contrat
         Me.GroupBox3.BackColor = System.Drawing.Color.SteelBlue
         Me.GroupBox3.Controls.Add(Me.BtnQuitter)
         Me.GroupBox3.Controls.Add(Me.BtnRenouveller)
-        Me.GroupBox3.Location = New System.Drawing.Point(0, 416)
-        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox3.Location = New System.Drawing.Point(0, 338)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox3.Size = New System.Drawing.Size(998, 132)
+        Me.GroupBox3.Size = New System.Drawing.Size(748, 107)
         Me.GroupBox3.TabIndex = 44
         Me.GroupBox3.TabStop = False
         '
         'BtnQuitter
         '
         Me.BtnQuitter.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
-        Me.BtnQuitter.Location = New System.Drawing.Point(727, 42)
-        Me.BtnQuitter.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnQuitter.Location = New System.Drawing.Point(545, 34)
         Me.BtnQuitter.Name = "BtnQuitter"
-        Me.BtnQuitter.Size = New System.Drawing.Size(180, 46)
+        Me.BtnQuitter.Size = New System.Drawing.Size(135, 37)
         Me.BtnQuitter.TabIndex = 41
         Me.BtnQuitter.Text = "Quitter"
         Me.BtnQuitter.UseVisualStyleBackColor = True
@@ -115,10 +107,9 @@ Partial Class Renouveler_Contrat
         'BtnRenouveller
         '
         Me.BtnRenouveller.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
-        Me.BtnRenouveller.Location = New System.Drawing.Point(39, 42)
-        Me.BtnRenouveller.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnRenouveller.Location = New System.Drawing.Point(29, 34)
         Me.BtnRenouveller.Name = "BtnRenouveller"
-        Me.BtnRenouveller.Size = New System.Drawing.Size(180, 46)
+        Me.BtnRenouveller.Size = New System.Drawing.Size(135, 37)
         Me.BtnRenouveller.TabIndex = 41
         Me.BtnRenouveller.Text = "Renouveler"
         Me.BtnRenouveller.UseVisualStyleBackColor = True
@@ -128,10 +119,9 @@ Partial Class Renouveler_Contrat
         Me.Titre.BackColor = System.Drawing.Color.DodgerBlue
         Me.Titre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Titre.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Titre.Location = New System.Drawing.Point(249, 5)
-        Me.Titre.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Titre.Location = New System.Drawing.Point(187, 4)
         Me.Titre.Name = "Titre"
-        Me.Titre.Size = New System.Drawing.Size(511, 28)
+        Me.Titre.Size = New System.Drawing.Size(384, 23)
         Me.Titre.TabIndex = 43
         Me.Titre.Text = "La liste des contrats"
         Me.Titre.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -140,11 +130,10 @@ Partial Class Renouveler_Contrat
         '
         Me.CONTRATDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.CONTRATDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Num_Contrat, Me.Nom, Me.Prénom, Me.Type_Contrat, Me.Equipements, Me.Column1, Me.column2})
-        Me.CONTRATDataGridView.Location = New System.Drawing.Point(9, 37)
-        Me.CONTRATDataGridView.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.CONTRATDataGridView.Location = New System.Drawing.Point(7, 30)
         Me.CONTRATDataGridView.Name = "CONTRATDataGridView"
         Me.CONTRATDataGridView.RowHeadersWidth = 51
-        Me.CONTRATDataGridView.Size = New System.Drawing.Size(989, 204)
+        Me.CONTRATDataGridView.Size = New System.Drawing.Size(742, 166)
         Me.CONTRATDataGridView.TabIndex = 42
         '
         'Num_Contrat
@@ -198,14 +187,13 @@ Partial Class Renouveler_Contrat
         '
         'Renouveler_Contrat
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1000, 546)
+        Me.ClientSize = New System.Drawing.Size(752, 444)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.Titre)
         Me.Controls.Add(Me.CONTRATDataGridView)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Renouveler_Contrat"
         Me.Text = "Renouveler_Contrat"
         Me.GroupBox2.ResumeLayout(False)
